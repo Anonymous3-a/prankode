630f5e458f77d66168d36829776af8de6a3a41d8
Anonymouse Coder <74880413+Anonymous3-a@users.noreply.github.com>
2021-12-05 15:48:28 -0800

    Added some comments
    I also commented out some code that wouldn't work

--- functions.js
+++ functions.js
@@ -3,16 +3,20 @@ function delall() {
 }
 
 function lagout() {
-	while (true) {}
+	while (true) {} // BE CAREFUL
 }
 
-function spin(speed, twodir) {
+/*
+As it turns out I made an oopsy, so if anyone wats to fix it, just submit an issue or pull request or whatever you do to contribute
+Oh yeah it was the speed idk how to use it like that
+function spin(speed, twodir) { // Speed is how long it takes to go around the screen
 	if(twodir) {
 		document.querySelector('body').innerHTML += '<style>body{animation: spin speed infinite;}@keyframes spin {0% {transform: rotate(0deg);}50% {transform: rotate(360deg);}100% {transform: rotate(0deg);}}</style>';
 	} else {
 		document.querySelector('body').innerHTML += '<style>body{animation: spin speed infinite;}@keyframes spin {from {transform: rotate(0deg);}to {transform: rotate(360deg);}}</style>';
 	}
 }
+*/
 
 function upsidedown() {
 	document.querySelector('body').innerHTML += '<style>body{transform: rotate(180deg);}</style>';
